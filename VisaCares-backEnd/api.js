9f32680b181e4802195afbadac820848316bc956
Zhihao Zhou <redickhao@gmail.com>
2020-06-22 14:40:58 -0700

    deleted sample code

--- VisaCares-backEnd/api.js
+++ VisaCares-backEnd/api.js
@@ -2,14 +2,11 @@ let express = require("express");
 
 let apiRouter = express.Router();
 
-let sampleController = require("./controller/sampleController");
 
 let donationController = require("./controller/donationController")
 
-apiRouter.get("/sampleAPI", sampleController.querySampleController);
 
 apiRouter.get("/donate", donationController.donateProcess);
 
-//  /api/sampleAPI
 
 module.exports = apiRouter;
