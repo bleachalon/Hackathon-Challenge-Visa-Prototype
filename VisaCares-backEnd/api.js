f666b82bdcdb88545134ddf8f4316c535c4a9fa5
Zhihao Zhou <redickhao@gmail.com>
2020-06-21 23:12:10 -0700

    Finish Chrome Extension and API

--- VisaCares-backEnd/api.js
+++ VisaCares-backEnd/api.js
@@ -4,6 +4,12 @@ let apiRouter = express.Router();
 
 let sampleController = require("./controller/sampleController");
 
+let donationController = require("./controller/donationController")
+
 apiRouter.get("/sampleAPI", sampleController.querySampleController);
+
+apiRouter.get("/donate", donationController.donateProcess);
+
+//  /api/sampleAPI
 
 module.exports = apiRouter;
